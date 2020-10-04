e7d970481e94796417831edf141a5690a6f7d04b
marina <marina.kalmykova@storytel.com>
2020-10-04 14:12:35 +0200

    добавление новых компонент и роутинга

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
+import Favorites from "../favorites/favorites";
+import SignIn from "../SignIn/signin";
+import Property from "../Property/property";
 
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 const App = (props) => {
-  const placeNumber = props;
+  const {placeNumber} = props;
   return (
-    <Main
-      placeNamber = {placeNumber}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            placeNumber = {placeNumber}
+          />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/property">
+          <Property />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
